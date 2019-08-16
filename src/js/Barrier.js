b5cd425dd0de17beceb1e9be87d8776853276880
Nout Kleef <noutkleef1@gmail.com>
2019-08-16 02:37:25 +0200

    rewrites Barrier.js to OOP

--- src/js/Barrier.js
+++ src/js/Barrier.js
@@ -1,32 +1,36 @@
-function Barrier(x, y, z, xv, yv, zv, width, height, texture, origW, origH) {
-    this.texture = texture;
-    this.jumped = false;
-    this.origW = origW;
-    this.origH = origH;
-    this.initialP = {
-        x: x,
-        y: y,
-        z: z
-    };
-    this.p = {
-        x: x,
-        y: y,
-        z: z
-    };
-    this.v = {
-        x: xv,
-        y: yv,
-        z: zv
-    };
-    this.width = width / z;
-    this.height = height / z;
-    this.show = function () {
+class Barrier {
+    constructor(x, y, z, xv, yv, zv, width, height, texture, origW, origH) {
+        this.texture = texture;
+        this.jumped = false;
+        this.origW = origW;
+        this.origH = origH;
+        this.initialP = {
+            x: x,
+            y: y,
+            z: z
+        };
+        this.p = {
+            x: x,
+            y: y,
+            z: z
+        };
+        this.v = {
+            x: xv,
+            y: yv,
+            z: zv
+        };
+        this.width = width / z;
+        this.height = height / z;
+    }
+
+    show() {
         this.texture.show(this.p.x, this.p.y, this.origW, this.origH);
     }
-    this.update = function () {
+
+    update() {
         this.p.x += this.v.x;
         this.p.y += this.v.y;
         this.p.z += this.v.z;
-        this.v.x = Math.abs(this.p.x - this.initialP.x) > MOVEMENT ? this.v.x * -1 : this.v.x;
+        this.v.x = Math.abs(this.p.x - this.initialP.x) > BlueJumpGame.MOVEMENT ? this.v.x * -1 : this.v.x;
     }
 }
