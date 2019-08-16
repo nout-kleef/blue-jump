cc3a593d1ab72711b052fc39dfe00ace655bd564
Nout Kleef <noutkleef1@gmail.com>
2019-08-16 02:34:08 +0200

    rewrites Floor.js to OOP

--- src/js/Floor.js
+++ src/js/Floor.js
@@ -1,9 +1,18 @@
-function Floor(img, imgWidth, imgHeight, frames, action) {
-    this.image = new SpriteAnimation(img, imgWidth, imgHeight, frames, action, 0, 0, 30);
-    this.repeats = Math.ceil(window.screen.width / imgWidth);
-    this.show = function (origW, origH) {
-        for (var i = 0; i < this.repeats; i++) {
-            this.image.show(i * this.image.width, (isSafari ? windowHeight : window.screen.height) - this.image.height, origW, origH);
-        }
+class Floor {
+    constructor(img, displayWidth, displayHeight, framesCount, action) {
+        this.sprite = new SpriteAnimation(
+            img, displayWidth, displayHeight, framesCount, action, 0, 0, 30
+        );
+        this.repeats = Math.ceil(window.screen.width / displayWidth);
+    }
+
+    show(origWidth, origHeight) {
+        for (let i = 0; i < this.repeats; i++)
+            this.image.show(
+                i * this.image.width,
+                (BlueJumpGame.IS_SAFARI ? windowHeight : window.screen.height) - this.image.height,
+                origWidth,
+                origHeight
+            );
     }
 }
